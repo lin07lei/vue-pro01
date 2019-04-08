a1e07fdd53031c427132e5068a111cecbace11d7
lin <yanglei@lindeMacBook-Pro.local>
2019-04-08 23:04:44 +0800

    路由模块抽取

--- src/main.js
+++ src/main.js
@@ -1,57 +1,10 @@
 import Vue from 'vue'
+import router from './router'
 import Layout from './components/layout'
-import VueRouter from 'vue-router'
-import IndexPage from './pages/index'
-import DetailPage from './pages/detail'
-import DetailAnaPage from './pages/detail/analysis'
-import DetailCouPage from './pages/detail/count'
-import DetailForPage from './pages/detail/forecast'
-import DetailPubPage from './pages/detail/publish'
-import Mockdata from "./mock/index"
-
-Vue.use(VueRouter)
-
-let router = new VueRouter({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      component: IndexPage
-    },
-    {
-      path: '/detail',
-      component: DetailPage,
-      redirect: '/detail/analysis',
-      children: [
-        {
-          path: '/analysis',
-          name: 'analysis',
-          component: DetailAnaPage
-        },
-        {
-          path: '/count',
-          name: 'count',
-          component: DetailCouPage
-        },
-        {
-          path: '/forecast',
-          name: 'forecast',
-          component: DetailForPage
-        },
-        {
-          path: '/publish',
-          name: 'publish',
-          component: DetailPubPage
-        }
-      ]
-    }
-  ]
-})
+// import Mockdata from "./mock/index"
 
 new Vue({
   el: '#app',
-  router,
-  // render: h => h(Layout),
-  template: '<layout/>',
-  components: { Layout }
+  router,  // 将路由注入实例
+  render: h => h(Layout)
 })
